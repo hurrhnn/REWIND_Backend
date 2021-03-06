099989a15673dd401d7cb07bea76676e79401586
hurrhnn <40728528+hurrhnn@users.noreply.github.com>
2021-03-06 22:39:28 +0900

    Added jwt_encode().

--- util.py
+++ util.py
@@ -4,7 +4,11 @@ import base64
 
 
 def b64_fix(data):
-    return data + b"="*((4 - len(data) % 4) if len(data) % 4 != 0 else 0)
+    return data + b"=" * ((4 - len(data) % 4) if len(data) % 4 != 0 else 0)
+
+
+def b64_rem(data):
+    return data.rstrip("=")
 
 
 def jwt_decode(jwt, secret=b"testsecretkey"):
@@ -15,4 +19,13 @@ def jwt_decode(jwt, secret=b"testsecretkey"):
     header, body = map(json.loads, (header, body))
     if hmac.digest(secret, body_verify.encode(), digest="sha256") != sig:
         return False
+    print(jwt_encode(body))
     return body
+
+
+def jwt_encode(body, secret=b"testsecretkey"):
+    body_verify = base64.urlsafe_b64encode(bytes('{"alg":"HS256","typ":"JWT"}', "UTF-8")).decode("UTF-8") \
+                  + '.' \
+                  + base64.urlsafe_b64encode(bytes((json.dumps(body, separators=(',', ':'))), "UTF-8")).decode("UTF-8")
+    sig = base64.urlsafe_b64encode(hmac.digest(secret, body_verify.rstrip("=").encode(), digest="sha256"))
+    return b64_rem(body_verify) + '.' + b64_rem(sig.decode("UTF-8"))
