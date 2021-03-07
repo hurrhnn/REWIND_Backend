0b00764bd703e757d9f45e86b33adaa1b850e6ec
lnJoy <50934835+lnJoy@users.noreply.github.com>
2021-03-07 14:22:39 +0900

    Rename database.py -> initialize_db.py, and add flask_sqlalchemy

--- db/initialize_db.py
+++ db/initialize_db.py
@@ -2,13 +2,15 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import scoped_session, sessionmaker
 
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
 engine = create_engine('sqlite:///WIND.db', convert_unicode=True)
 db_session = scoped_session(sessionmaker(autocommit=False,
                                          autoflush=False,
                                          bind=engine))
 Base = declarative_base()
 Base.query = db_session.query_property()
-
 
 def init_db():
     Base.metadata.create_all(bind=engine)
