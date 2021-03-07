fcd31ee91971f11142b1dace667d4e6744629b60
lnJoy <50934835+lnJoy@users.noreply.github.com>
2021-03-07 14:33:45 +0900

    Deleted /login, /register api code, and add API

--- web/views/index.py
+++ web/views/index.py
@@ -1,9 +1,9 @@
-from flask import Blueprint, request, Response, redirect, render_template, session, url_for, json, flash
+from flask import Blueprint, redirect, render_template, session, url_for, request, Response, flash
 
 from flask_sqlalchemy import SQLAlchemy
 from websocket.model import *
 
-from db.database import db_session
+from db.initialize_db import db_session
 
 from util import jwt_encode
 
@@ -23,53 +23,13 @@ def index():
         return redirect(url_for('index.login'))
     return render_template('index.html')
 
-
-@bp.route('/login', methods=['GET', 'POST'])
+@bp.route('/login')
 def login():
-    if request.method == 'GET':
-        return render_template('login.html')
-    else:
-        try:
-            email = request.form['email']
-            pwd = request.form['password']
-            data = User.query.filter_by(email=email, password=pwd).first()
+    return render_template('login.html')
 
-            token = jwt_encode(json.loads(str(data)))
-
-            obj = json.dumps({'type': 'authentication', 'payload': {'token': token}}, ensure_ascii=False).encode('utf8')
-
-            if data is not None:
-                session['logined'] = True
-                return Response(response=obj, status=201, content_type='application/json')
-            else:
-                return Response(status=401)
-        except Exception as e:
-            print(e)
-            return Response(status=401)
-
-
-@bp.route('/register', methods=['GET', 'POST'])
+@bp.route('/register')
 def register():
-    if request.method == 'POST':
-        try:
-            name = request.form['name']
-            email = request.form['email']
-            password = request.form['password']
-
-            new_user = User(name=name, email=email, password=password)
-
-            if User.query.filter(User.email == email).first() is not None:
-                flash('이메일이 이미 존재합니다')
-                return render_template('register.html')
-
-            db_session.add(new_user)
-            db_session.commit()
-            return redirect(url_for('index.login'))
-        except Exception as e:
-            print(e)
-            return Response(status=401)
     return render_template('register.html')
-
 
 @bp.route('/logout')
 def logout():
