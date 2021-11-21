b7ef037c1bb10384419732f61620f0d2bd59e60d
hurrhnn <20sunrin022@sunrint.hs.kr>
2021-11-21 16:59:39 +0900

    Added some json structure.

--- websocket/util.py
+++ websocket/util.py
@@ -1,11 +1,22 @@
 import json
 
 
-def get_data(type, payload):
+def get_data(_type, payload):
     return json.dumps({
-        "type": type,
+        "type": _type,
         "payload": payload
     }).encode("utf-8")
+
+
+def handshake(sess_data, friends):
+    return get_data("handshake", {
+        "user_info": sess_data['user'],
+        "friends": friends
+    })
+
+
+def heartbeat(payload):
+    return get_data("heartbeat", payload)
 
 
 def error(code, reason):
@@ -15,10 +26,21 @@ def error(code, reason):
     })
 
 
-def chat(type, user_id, chat_id, content):
+def chat(_type, _id, user_id, chat_id, content):
     return get_data("chat", {
-        "type": type,
+        "type": _type,
+        "id": _id,
         "user_id": user_id,
         "chat_id": chat_id,
         "content": content
+    })
+
+
+def load(_type, _id, user_id, load_id, count):
+    return get_data("load", {
+        "type": _type,
+        "id": _id,
+        "user_id": user_id,
+        "load_id": load_id,
+        "count": count
     })
