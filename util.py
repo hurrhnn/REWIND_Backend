ed5f13136b7a0a6566c80c847cd7a88f5ed4c85f
hurrhnn <20sunrin022@sunrint.hs.kr>
2021-12-01 07:31:47 +0900

    Changed secret keys.

--- util.py
+++ util.py
@@ -8,15 +8,15 @@ def generate_snowflake(session=None):
     from web import db
     from db.models import ModelCreator
 
-    Counter = ModelCreator.get_model("counter")
+    counter = ModelCreator.get_model("counter")
     if session is None:
         # this is flask!
-        counter = Counter.query.first()
+        counter = counter.query.first()
         counter.count += 1
         db.session.commit()
     else:
         # this is websocket!
-        counter = session.query(Counter).first()
+        counter = session.query(counter).first()
         counter.count += 1
         session.commit()
 
@@ -33,24 +33,25 @@ def b64_rem(data):
     return data.rstrip("=")
 
 
-def jwt_decode(jwt, secret=b"testsecretkey"):
+def jwt_decode(jwt):
     body_verify = jwt.rsplit(".", 1)[0]
     header, body, sig = [
         base64.urlsafe_b64decode(b64_fix(data.encode()))
         for data in jwt.split(".")]
     header, body = map(json.loads, (header, body))
-    if hmac.digest(secret, body_verify.encode(), digest="sha256") != sig:
+    if hmac.digest(secret(True), body_verify.encode(), digest="sha256") != sig:
         return False
-    return body
+    else:
+        return body
 
 
-def jwt_encode(body, secret=b"testsecretkey"):
+def jwt_encode(body):
     body_verify = base64.urlsafe_b64encode(bytes('{"alg":"HS256","typ":"JWT"}', "UTF-8")).decode("UTF-8") \
                   + '.' \
                   + base64.urlsafe_b64encode(bytes((json.dumps(body, separators=(',', ':'))), "UTF-8")).decode("UTF-8")
-    sig = base64.urlsafe_b64encode(hmac.digest(secret, body_verify.rstrip("=").encode(), digest="sha256"))
+    sig = base64.urlsafe_b64encode(hmac.digest(secret(True), body_verify.rstrip("=").encode(), digest="sha256"))
     return b64_rem(body_verify) + '.' + b64_rem(sig.decode("UTF-8"))
 
 
-def secret():
-    return jwt_encode({"id": -99999, "name": "SecretAdmin"})
+def secret(sig=False):
+    return jwt_encode({"id": 0, "name": "Admin", "profile": None}) if not sig else "1084633200".encode()
