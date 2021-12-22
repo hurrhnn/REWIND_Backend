b1dc4a749fff68f4ae6fc3f0868c6b7c80a76a47
hurrhnn <20sunrin022@sunrint.hs.kr>
2021-12-23 06:36:12 +0900

    Fixed SQLite3 error that checking same thread.

--- db/config.py
+++ db/config.py
@@ -3,8 +3,8 @@ import os
 PATH = os.path.dirname(os.path.abspath(__file__))
 
 SQLALCHEMY_BINDS = {
-    'main': f'sqlite:///{PATH}/WIND_main.db',
-    'chat': f'sqlite:///{PATH}/WIND_chat.db'
+    'main': f'sqlite:///{PATH}/WIND_main.db?check_same_thread=False',
+    'chat': f'sqlite:///{PATH}/WIND_chat.db?check_same_thread=False'
 }
 
 del os, PATH
